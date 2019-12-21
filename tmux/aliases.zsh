035a22579a50abcf3bbe6e44f3f48a4317bdacf8
nguyentodo <nguyen0591@gmail.com>
2019-12-21 15:51:32 +0700

    Update tmux alias

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 alias tka='tmux kill-session -t .'
-alias td='tmux new-session -d -s DEV && source $HOME/.tmux/powerline-theme.tmux && tmux attach -t DEV'
+alias ta='tmux attach'
+alias td='tmux new-session -d -s dev && source $HOME/.tmux/powerline-theme.tmux && tmux attach -t dev'
